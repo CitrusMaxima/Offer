50f3f782f9de181bc6d770c190c784dfe6185f0e
CitrusMaxima <2663753900@qq.com>
2018-06-21 21:12:42 +0800

    19.顺时针打印矩阵

--- src/offer/Solution.java
+++ src/offer/Solution.java
@@ -234,7 +234,7 @@ public class Solution {
         return res;
     }
 
-    public double mutiply(double base, int e) {
+    private double mutiply(double base, int e) {
         double sum = 1;
         for (int i = 0; i < e; i++) {
             sum = sum * base;
@@ -341,7 +341,7 @@ public class Solution {
         return result;
     }
 
-    public boolean doesTree1HaveTree2(TreeNode node1, TreeNode node2) {
+    private boolean doesTree1HaveTree2(TreeNode node1, TreeNode node2) {
         if (node2 == null) {
             return true;
         }
@@ -368,5 +368,45 @@ public class Solution {
         }
     }
 
+    // 19.顺时针打印矩阵
+    public ArrayList<Integer> printMatrix(int[][] matrix) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if (matrix == null)
+            return list;
+        int start = 0;
+        while (matrix[0].length > start * 2 && matrix.length > start * 2) {
+            printOneCircle(matrix, start, list);
+            start++;
+        }
+        return list;
+    }
+
+    private void printOneCircle(int[][] matrix, int start, ArrayList<Integer> list) {
+        int endX = matrix[0].length - 1 - start; // 列
+        int endY = matrix.length - 1 - start;  // 行
+
+        // 从左往右
+        for (int i = start; i <= endX; ++i) {
+            list.add(matrix[start][i]);
+        }
+        // 从上往下
+        if (start < endY) {
+            for (int i = start + 1; i <= endY; ++i) {
+                list.add(matrix[i][endX]);
+            }
+        }
+        // 从右往左（判断是否会重复打印）
+        if (start < endX && start < endY) {
+            for (int i = endX - 1; i >= start; --i) {
+                list.add(matrix[endY][i]);
+            }
+        }
+        // 从下往上（判断是否会重复打印）
+        if (start < endX && start < endY - 1) {
+            for (int i = endY - 1; i >= start + 1; --i) {
+                list.add(matrix[i][start]);
+            }
+        }
+    }
 
 }
