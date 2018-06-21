0dacf78ade63dc87406dd149595d52f9628e201a
CitrusMaxima <2663753900@qq.com>
2018-06-21 20:02:40 +0800

    14.链表中倒数第k个节点

--- src/offer/Solution.java
+++ src/offer/Solution.java
@@ -267,4 +267,25 @@ public class Solution {
         }
     }
 
+    // 14.链表中倒数第k个节点
+    public ListNode FindKthToTail(ListNode head, int k) {
+        if (head == null || k <= 0)
+            return null;
+
+        ListNode fast = head;
+        ListNode slow = head;
+        while (k-- > 1) {
+            if (fast.next != null)
+                fast = fast.next;
+            else
+                return null;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+
 }
