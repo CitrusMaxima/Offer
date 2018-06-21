774687fa5b90a32d5e39e4cb1cfe51261392603c
CitrusMaxima <2663753900@qq.com>
2018-06-21 20:29:08 +0800

    17.树的子结构

--- src/offer/Solution.java
+++ src/offer/Solution.java
@@ -324,6 +324,35 @@ public class Solution {
         return newHead;
     }
 
+    // 17.树的子结构
+    public boolean HasSubtree(TreeNode root1, TreeNode root2) {
+        boolean result = false;
 
+        if (root1 != null && root2 != null) {
+            if (root1.val == root2.val) {
+                result = doesTree1HaveTree2(root1, root2);
+            }
+
+            if (!result) {
+                return HasSubtree(root1.left, root2) || HasSubtree(root1.right, root2);
+            }
+        }
+
+        return result;
+    }
+
+    public boolean doesTree1HaveTree2(TreeNode node1, TreeNode node2) {
+        if (node2 == null) {
+            return true;
+        }
+        if (node1 == null) {
+            return false;
+        }
+        if (node1.val != node2.val) {
+            return false;
+        }
+        return doesTree1HaveTree2(node1.left, node2.left) &&
+                doesTree1HaveTree2(node1.right, node2.right);
+    }
 
 }
