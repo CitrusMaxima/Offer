a93a10062d561630b47138e07c62631c6da3a047
CitrusMaxima <2663753900@qq.com>
2018-06-21 19:21:56 +0800

    9.变态跳台阶

--- src/offer/Solution.java
+++ src/offer/Solution.java
@@ -184,4 +184,9 @@ public class Solution {
         }
         return result;
     }
+
+    // 9.变态跳台阶
+    public int JumpFloorII(int target) {
+        return (int) Math.pow(2,target-1);
+    }
 }
