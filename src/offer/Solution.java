f7beb9c0a369a9cd86ba199d4b236fa6682b6e8a
CitrusMaxima <2663753900@qq.com>
2018-06-27 21:33:25 +0800

    23.二叉搜索树的后序遍历序列

--- src/offer/Solution.java
+++ src/offer/Solution.java
@@ -479,4 +479,30 @@ public class Solution {
         return list;
     }
 
+    // 23.二叉搜索树的后序遍历序列
+    public boolean VerifySquenceOfBST(int[] sequence) {
+        if (sequence == null || sequence.length == 0)
+            return false;
+
+        int rStart = 0;
+        int length = sequence.length;
+
+        for (int i = 0; i < length - 1; ++i) {
+            if (sequence[i] < sequence[length - 1])
+                rStart++;
+        }
+
+        if (rStart == 0) {
+            VerifySquenceOfBST(Arrays.copyOfRange(sequence, 0, length - 1));
+        } else {
+            for (int i = rStart; i < length - 1; ++i) {
+                if (sequence[i] <= sequence[length - 1])
+                    return false;
+            }
+            VerifySquenceOfBST(Arrays.copyOfRange(sequence, 0, rStart));
+            VerifySquenceOfBST(Arrays.copyOfRange(sequence, rStart, length - 1));
+        }
+        return true;
+    }
+
 }
