6c67990a981fda9fa5363085b896e5e936c9f872
CitrusMaxima <2663753900@qq.com>
2018-06-27 20:11:09 +0800

    22.从上往下打印二叉树

--- src/offer/Main.java
+++ src/offer/Main.java
@@ -4,8 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] array = new int[]{2,7,4,1,3,9,2};
-        solution.reOrderArray(array);
+        TreeNode treeNode = new TreeNode(5);
+        treeNode.left = new TreeNode(2);
+        treeNode.right = new TreeNode(8);
+        treeNode.left.left = new TreeNode(1);
+        treeNode.right.left = new TreeNode(12);
+        System.out.println("result:" + solution.PrintFromTopToBottom(treeNode));
 
     }
 }
