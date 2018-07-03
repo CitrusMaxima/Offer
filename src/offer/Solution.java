bb558900ee72194ef27dc2c45c9f08055b333404
CitrusMaxima <2663753900@qq.com>
2018-07-03 20:54:19 +0800

    // 36.两个链表的第一个公共节点

--- src/offer/Solution.java
+++ src/offer/Solution.java
@@ -838,6 +838,15 @@ public class Solution {
         }
     }
 
-
+    // 36.两个链表的第一个公共节点
+    public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+        ListNode p1 = pHead1;
+        ListNode p2 = pHead2;
+        while (p1 != p2){
+            p1 = (p1 != null ? p1.next : pHead2);
+            p2 = (p2 != null ? p2.next : pHead1);
+        }
+        return p1;
+    }
 
 }
