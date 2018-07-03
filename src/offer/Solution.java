8962eb8d3901b6678e84e5e568af24c2927bcf42
CitrusMaxima <2663753900@qq.com>
2018-07-03 20:05:39 +0800

    33.丑数

--- src/offer/Solution.java
+++ src/offer/Solution.java
@@ -740,4 +740,27 @@ public class Solution {
         return stringbuilder.toString();
     }
 
+    // 33.丑数
+    public int GetUglyNumber_Solution(int index) {
+        if (index <= 0)
+            return 0;
+        int[] arr = new int[index];
+        arr[0] = 1;
+        int multiply2 = 0;
+        int multiply3 = 0;
+        int multiply5 = 0;
+
+        for (int i = 1; i < index; i++) {
+            int min = Math.min(arr[multiply2] * 2, Math.min(arr[multiply3] * 3, arr[multiply5] * 5));
+            arr[i] = min;
+            if (arr[multiply2] * 2 == min)
+                multiply2++;
+            if (arr[multiply3] * 3 == min)
+                multiply3++;
+            if (arr[multiply5] * 5 == min)
+                multiply5++;
+        }
+        return arr[index - 1];
+    }
+
 }
