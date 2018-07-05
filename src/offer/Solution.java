1fcae196213405e271dfc20822b7222a83e705bd
CitrusMaxima <2663753900@qq.com>
2018-07-05 21:22:42 +0800

    39.平衡二叉树

--- src/offer/Solution.java
+++ src/offer/Solution.java
@@ -911,4 +911,24 @@ public class Solution {
         return left > right ? left + 1 : right + 1;
     }
 
+    // 39.平衡二叉树
+    public boolean IsBalanced_Solution(TreeNode root) {
+        return getDepth(root) != -1;
+    }
+
+    private int getDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int left = getDepth(root.left);
+        if (left == -1)
+            return -1;
+
+        int right = getDepth(root.right);
+        if (right == -1)
+            return -1;
+
+        return Math.abs(left - right) > 1 ? -1 : 1 + Math.max(left, right);
+    }
+
 }
