5e16cb652969af4ff3f2cc9e0863e72bae593c42
CitrusMaxima <2663753900@qq.com>
2018-07-06 11:16:55 +0800

    41.和为S的连续正数序列

--- src/offer/Solution.java
+++ src/offer/Solution.java
@@ -967,4 +967,18 @@ public class Solution {
         return ((target >> index) & 1) == 1;
     }
 
+    // 41.和为S的连续正数序列
+    public ArrayList<ArrayList<Integer>> FindContinuousSequence(int sum) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        for (int n = (int) Math.sqrt(2 * sum); n >= 2; --n) {
+            if ((n & 1) == 1 && sum % n == 0 || (sum % n) * 2 == n) {
+                ArrayList<Integer> list = new ArrayList<>();
+                for (int j = 0, k = (sum / n) - (n - 1) / 2; j < n; ++j, ++k)
+                    list.add(k);
+                result.add(list);
+            }
+        }
+        return result;
+    }
+
 }
