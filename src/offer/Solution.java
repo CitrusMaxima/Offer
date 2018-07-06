2330b1989592f96a2d1126ae1f298b1d763e86e8
CitrusMaxima <2663753900@qq.com>
2018-07-06 20:37:11 +0800

    42.和为S的两个数字

--- src/offer/Solution.java
+++ src/offer/Solution.java
@@ -981,4 +981,24 @@ public class Solution {
         return result;
     }
 
+    // 42.和为S的两个数字
+    public ArrayList<Integer> FindNumbersWithSum(int [] array,int sum) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if (array == null || array.length < 2)
+            return list;
+
+        int i = 0, j = array.length - 1;
+        while (i < j) {
+            if (array[i] + array[j] == sum) {
+                list.add(array[i]);
+                list.add(array[j]);
+                return list;
+            } else if (array[i] + array[j] > sum) {
+                --j;
+            } else
+                ++i;
+        }
+        return list;
+    }
+
 }
