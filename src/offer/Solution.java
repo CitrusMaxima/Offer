f971418bc3d9ee70c3f08bfae5aa55324c97dd41
CitrusMaxima <2663753900@qq.com>
2018-07-06 10:21:40 +0800

    40.数组中只出现一次的数字

--- src/offer/Solution.java
+++ src/offer/Solution.java
@@ -931,4 +931,40 @@ public class Solution {
         return Math.abs(left - right) > 1 ? -1 : 1 + Math.max(left, right);
     }
 
+    // 40.数组中只出现一次的数字
+    public void FindNumsAppearOnce(int[] array, int num1[], int num2[]) {
+        int length = array.length;
+        if (length == 2) {
+            num1[0] = array[0];
+            num2[0] = array[1];
+            return;
+        }
+
+        int bitResult = 0;
+        for (int i = 0; i < length; ++i) {
+            bitResult ^= array[i];
+        }
+
+        int index = findFirst1(bitResult);
+        for (int i = 0; i < length; ++i) {
+            if (isBit1(array[i], index))
+                num1[0] ^= array[i];
+            else
+                num2[0] ^= array[i];
+        }
+    }
+
+    private int findFirst1(int bitResult) {
+        int index = 0;
+        while ((bitResult & 1) == 0 && index < 32) {
+            bitResult >>= 1;
+            index++;
+        }
+        return index;
+    }
+
+    private boolean isBit1(int target, int index) {
+        return ((target >> index) & 1) == 1;
+    }
+
 }
