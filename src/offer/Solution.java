4e8a5b6a2494d596becb1fb187b5227beef8b749
CitrusMaxima <2663753900@qq.com>
2018-08-12 15:52:12 +0800

    49.数组中重复的数字

--- src/offer/Solution.java
+++ src/offer/Solution.java
@@ -1096,4 +1096,20 @@ public class Solution {
         return num1;
     }
 
+    // 49.数组中重复的数字
+    public boolean duplicate(int numbers[], int length, int[] duplication) {
+        for (int i = 0; i < length; ++i) {
+            int index = numbers[i];
+            if (index >= length) {
+                index -= length;
+            }
+            if (numbers[index] >= length) {
+                duplication[0] = index;
+                return true;
+            }
+            numbers[index] = numbers[index] + length;
+        }
+        return false;
+    }
+    
 }
