fe51728aebe431a3147030b6e7cb1a07e1563348
CitrusMaxima <2663753900@qq.com>
2018-08-12 15:40:33 +0800

    48.不用加减乘除做加法

--- src/offer/Solution.java
+++ src/offer/Solution.java
@@ -1086,4 +1086,14 @@ public class Solution {
         }
     }
 
+    // 48.不用加减乘除做加法
+    public int Add(int num1, int num2) {
+        while (num2 != 0) {
+            int temp = num1 ^ num2;
+            num2 = (num1 & num2) << 1;
+            num1 = temp;
+        }
+        return num1;
+    }
+
 }
